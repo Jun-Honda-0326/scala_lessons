939e9b7ba34ec98381ae69ffc676a4d425f8e9f2
Jun-Honda-0326 <60055417+Jun-Honda-0326@users.noreply.github.com>
2020-08-10 18:08:34 +0900

    hello_world

--- src/main/scala/MyApp.scala
+++ src/main/scala/MyApp.scala
@@ -1,7 +1,14 @@
 object MyApp {
 
   def main(args: Array[String]): Unit = {
-    println("Hello World")
+
+    // val 再代入できない
+    // var 再代入できる
+
+    var msg = "Hello World" //型推論
+    println(msg)
+     msg = "Hello World Again"
+    println(msg)
   }
 
 }
