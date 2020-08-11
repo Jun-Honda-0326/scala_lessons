8bc25ed5e2dd1e45b90369f008c110c2f71f9dad
Jun-Honda-0326 <60055417+Jun-Honda-0326@users.noreply.github.com>
2020-08-11 12:42:01 +0900

    ifを使った条件分岐

--- src/main/scala/MyApp.scala
+++ src/main/scala/MyApp.scala
@@ -2,16 +2,32 @@ object MyApp {
 
   def main(args: Array[String]): Unit = {
 
-    val x = 10
-    println(x / 3.0)
+    // val x = 10
+    // println(x / 3.0)
 
-    println(x % 3)
+    // println(x % 3)
 
-    var y = 5
-    y -= 2
-    println(y)
+    // var y = 5
+    // y -= 2
+    // println(y)
 
-    val flag = true
-    println(!flag)
+    // val flag = true
+    // println(!flag)
+
+    // val name = "taguchi"
+    // val score = 80
+    // val height = 158.3
+
+    // println(s"name:$name , score:$score , height:$height") //変数を埋め込みたい時は前にsを付与する
+    // println(s"name:$name , score:${score + 10 } , height:$height")
+    // println(f"name:$name%s , score:$score%-10d , height:$height%-1.2f") //10は最小表示桁数
+
+    val score = 70
+
+    val  result =  
+      if (score > 80) "great"
+      else if (score > 60) "good"
+      else "so so..."
+    println(result)
   }
 }
