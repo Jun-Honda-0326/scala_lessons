25de385e1b3113be40d37c0671dcc4a7c9f1eda2
Jun-Honda-0326 <60055417+Jun-Honda-0326@users.noreply.github.com>
2020-08-18 18:54:34 +0900

    set

--- src/main/scala/MyApp.scala
+++ src/main/scala/MyApp.scala
@@ -1,31 +1,11 @@
-<<<<<<< HEAD
  object MyApp {
 
    def msg(from: String, to: String, text: String) = s"($from -> $to): $text"
 
   def main(args: Array[String]): Unit = {
     val msgToFkoji = msg(_: String, "fkoji", _: String )
-=======
-
-class MyDate[T] {
-  def getThree(i: T ): Unit = println (s"$i, $i, $i") 
-}
-
-
-
->>>>>>> c0a08015175a0b0505a61b07c9108c38046121ec
 
     println(msgToFkoji("taguchi", "OK"))
-
-<<<<<<< HEAD
-    
-=======
-  def main(args: Array[String]): Unit = {
-    val  i = new MyDate[Int]
-    i.getThree(3)
-    val s = new MyDate[String]
-    s.getThree("hello")
->>>>>>> c0a08015175a0b0505a61b07c9108c38046121ec
   }
 }
 
