94300a2177574d1c1e27e64df0b96063361c9bae
Jun-Honda-0326 <60055417+Jun-Honda-0326@users.noreply.github.com>
2020-08-18 18:52:38 +0900

    set

--- src/main/scala/MyApp.scala
+++ src/main/scala/MyApp.scala
@@ -1,31 +1,21 @@
-<<<<<<< HEAD
  object MyApp {
 
-   def msg(from: String, to: String, text: String) = s"($from -> $to): $text"
+   def swap(a: Int, b: Int) = (b,a)
 
+   
+   
   def main(args: Array[String]): Unit = {
-    val msgToFkoji = msg(_: String, "fkoji", _: String )
-=======
 
-class MyDate[T] {
-  def getThree(i: T ): Unit = println (s"$i, $i, $i") 
-}
+    val set_1 = Set(5,3,8)
+    val set_2 = Set(5,1,2,8,10)
 
-
-
->>>>>>> c0a08015175a0b0505a61b07c9108c38046121ec
-
-    println(msgToFkoji("taguchi", "OK"))
-
-<<<<<<< HEAD
+    println(set_1 | set_2)
+    println(set_1 & set_2)
+    println(set_1 intersect set_2)
+    println(set_1 diff set_2)
+ 
     
-=======
-  def main(args: Array[String]): Unit = {
-    val  i = new MyDate[Int]
-    i.getThree(3)
-    val s = new MyDate[String]
-    s.getThree("hello")
->>>>>>> c0a08015175a0b0505a61b07c9108c38046121ec
+        
   }
 }
 
